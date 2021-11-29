5c52be88f641860cc558344c022ffc0450ce5f6f
Lesechko <alekseylesechko@gmail.com>
2021-11-29 16:46:33 +0200

    clean boilerplate

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,10 @@
 import Header from "./components/header/Header";
 
 const routes = [
-  // {
-  //   path: '/',
-  //   exact: true,
-  //   component: Home
-  // },
   {
     path: '/search',
     component: Header
   },
-  // {
-  //   path: '/user/:id',
-  //   component: User
-  // },
-  // {
-  //   path: '/about',
-  //   component: About
-  // },
 ];
 
 export default routes;
